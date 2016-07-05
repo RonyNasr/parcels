7cd8273e58405eded701dac97faf61f90d73e0d7
Caleb Stokka and Rony Nasr <caleb+rony@>
2016-07-05 10:39:24 -0700

    returns the cost to ship the parcel

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -10,4 +10,7 @@ class Parcel
   define_method(:volume) do
     @length * @height * @depth
   end
+  define_method(:cost_to_ship) do
+    @weight + (self.volume() * 0.5)
+  end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -8,4 +8,10 @@ describe(Parcel) do
       expect(test_parcel.volume()).to(eq(80))
     end
   end
+  describe('#cost_to_ship') do
+    it('returns the cost to ship the parcel') do
+      test_parcel = Parcel.new(5,2,8,10)
+      expect(test_parcel.cost_to_ship()).to(eq(50))
+    end
+  end
 end
